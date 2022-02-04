12f58012f222e7401744c20a5eb539ae99712d31
Chinecherem Harry <chinecherempharry@gmail.com>
2022-02-04 14:07:40 +0100

    product stats

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -5,14 +5,57 @@ import { productData } from "../../dummyData";
 import { Publish } from "@mui/icons-material";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 const Product = () => {
   const location = useLocation();
   const productId = location.pathname.split("/")[2];
+  const [productStats, setProductStats] = useState([]);
 
   const product = useSelector((state) =>
     state.product.products.find((product) => product._id === productId)
   );
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get(`orders/income?pid=`);
+        const list = res.data.sort((a, b) => {
+          return a._id - b._id;
+        });
+        console.log(res);
+        list.map((item) =>
+          setProductStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], Sales: item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [productId, MONTHS]);
+
   return (
     <div className="product">
       <div className="productTitleContainer">
@@ -24,7 +67,7 @@ const Product = () => {
       <div className="productTop">
         <div className="productTopLeft">
           <Chart
-            data={productData}
+            data={productStats}
             dataKey={"Sales"}
             title={"Sales Performance"}
           />
@@ -46,7 +89,7 @@ const Product = () => {
 
             <div className="productInfoItem">
               <span className="productInfoKey">in stock:</span>
-              <span className="productInfoValue">{product.stock}</span>
+              <span className="productInfoValue">{product.inStock}</span>
             </div>
           </div>
         </div>
@@ -55,26 +98,21 @@ const Product = () => {
         <form action="" className="productForm">
           <div className="productFormLeft">
             <label htmlFor="">Product Name</label>
-            <input type="text" placeholder="Apple Airpod" />
+            <input type="text" placeholder={product.title} />
+            <label htmlFor="">Product Description</label>
+            <input type="text" placeholder={product.desc} />
+            <label htmlFor="">Price</label>
+            <input type="text" placeholder={product.price} />
             <label>In Stock</label>
             <select name="inStock" id="idStock">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-            <label>Active</label>
-            <select name="active" id="active">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
             </select>
           </div>
           <div className="productFormRight">
             <div className="productUpload">
-              <img
-                src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
-                className="productUploadImg"
-              />
-              <label for="file">
+              <img src={product.img} alt="" className="productUploadImg" />
+              <label htmlFor="file">
                 <Publish />
               </label>
               <input type="file" id="file" style={{ display: "none" }} />
